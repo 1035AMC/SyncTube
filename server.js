528903ce6655aca344699743baddc37f7f6095c2
Arturo Mena Cruz <arturomenacruz@user3-208-175.wireless.utoronto.ca>
2014-09-18 17:44:03 -0400

    added functions to retrieve, post and delete todos

--- server.js
+++ server.js
@@ -8,14 +8,79 @@
 
 	app.use(express.logger());
 	app.set('port', (process.env.PORT || 5000))
-	app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-	app.use(morgan('dev')); 
-	app.use(bodyParser.urlencoded({'extended': 'true'}));
-	app.use(bodyParser.json()); 									// parse application/json
-	app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+	 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+	app.use(morgan('dev'));
+	app.use(bodyParser.urlencoded({
+	    'extended': 'true'
+	}));
+
+	app.use(bodyParser.json()); // parse application/json
+
+	app.use(bodyParser.json({
+	    type: 'application/vnd.api+json'
+	})); // parse application/vnd.api+json as json
+
 	app.use(methodOverride());
 
-	app.get("/", function(request, response) {});
 
-	app.listen(8080);
+	var retrieve_todos = function() {
+	    Todo.find(function(err, todo) {
+	        if (err) {
+	            res.send(err);
+	        } else {
+	            res.json(todos);
+	        };
+	    });
+	};
+
+	var Todo = mongoose.model("Todo", {
+	    text: String
+	});
+
+	app.get("/api/todos", function(req, res) {
+		retrieve_todos();
+	});
+
+
+	//This post creates the todo requested and then it shows the updated version of todos to the user.
+	app.post('/api/todos', function(req, res) {
+	    Todo.create({
+	        text: req.body.text;
+	        done: false
+	    }, function(err, todo) {
+	        if (err) {
+	            res.send(err);
+	        } else {
+	        	Todo.find(function(err,values){
+	        		if (err){
+	        			res.send(err);
+	        		}else{
+	        			res.json(values)
+	        		};
+	        	});
+
+	        };
+	    });
+	});
+
+	//delete a todo
+
+	app.delete('/api/todos/:todo_id', function(req, res){
+		Todo.remove({
+			_id : req.params.todo_id
+		}, function(err, todo){
+			if (err){
+				res.send(err);
+			}else{
+				Todo.find(err, values){
+					if (err){
+						res.send(err)
+					}
+					res.json(values);
+				};
+			};
+		});
+	});
+
+	 app.listen(8080);
 	console.log("App listening on port 8080");
