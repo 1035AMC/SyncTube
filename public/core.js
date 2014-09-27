8934b8d0c04017f26cc918a839fa7a1260a37304
1035AMC <arturo.1035@gmail.com>
2014-09-27 12:07:10 -0400

    updated

--- public/core.js
+++ public/core.js
@@ -1,9 +1,34 @@
 myApp = angular.module("syncTube")
 
-  myApp = angular.module('textInputExample', [])
 
+myApp.controller("mainController", function($scope, $http) {
+    $scope.formData = {};
 
-    myApp.controller('ExampleController', ['$scope', function($scope) {
-      $scope.text = 'guest';
-      $scope.word = /^\s*\w*\s*$/;
-    }]);
+    $http.get("/api/todos").success(function(data) {
+        $scope.todos = data;
+    }).error(function(err) {
+        console.log(err);
+    });
+
+    $scope.createTodo = function() {
+        $http.post("/api/todos", formData).success(function(data) {
+            $scope.formData = {};
+            $scope.todos = data;
+            console.log(data);
+        }).error(function(err) {
+            console.log(err);
+        });
+    };
+
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+});
